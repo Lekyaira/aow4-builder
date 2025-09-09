3458e8dd33031648193e208e21f61796db6d873c
Ryan Anderson <anderson.ryan.w@live.com>
2025-09-09 13:04:59 -0500

    Added debugging log exports

--- frontend/src/plugins/buildImport.ts
+++ frontend/src/plugins/buildImport.ts
@@ -2,11 +2,14 @@ import type { Router } from "vue-router";
 import { useEmpireStore } from "@/stores/empire";
 
 export function installBuildImport(router: Router) {
+  console.log("Running installBuildImport...");
   router.beforeEach((to, from, next) => {
     const maybeBuild = to.query.build;
+    console.log("maybeBuild:", maybeBuild);
     if (typeof maybeBuild === "string" && maybeBuild.length > 0) {
       const store = useEmpireStore();
       const ok = store.applyFromUrl(); // this also cleans the URL
+      console.log("Applied to store.");
       // Ensure we continue to intended route but without the query
       if (ok) {
         next({ ...to, query: { ...to.query, build: undefined } });
